7106fae36b3dd27d87313c6a9fd1fd0001c9fc79
Yoann Canal <ycanal@sophiacom.fr>
2015-05-25 18:51:18 +0200

    create a symlink for webresources

--- manifests/application.pp
+++ manifests/application.pp
@@ -1,21 +1,35 @@
-define wonder::application ($appname = $title, $binaryRelativePath = "${title}.woa", $username = $wonder::username) {
-	exec { "add application ${appname}":
+define wonder::application ($appname = $title, $binaryRelativePath = "${appname}.woa/${appname}", $username = $wonder::username) {
+	exec { "add application ${title}":
 		require => Exec['wait for monitor'],
-		command => "/usr/bin/curl -X POST -d \"{id: '${appname}',type: 'MApplication', name: '${appname}', unixOutputPath: '/home/${username}/logs', unixPath: '/home/${username}/apps/${binaryRelativePath}', autoRecover: false}\" http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/ra/mApplications.json"
+		command => "/usr/bin/curl -X POST -d \"{id: '${title}',type: 'MApplication', name: '${title}', unixOutputPath: '/home/${username}/logs', unixPath: '/home/${username}/apps/${binaryRelativePath}', autoRecover: false}\" http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/ra/mApplications.json"
 	}
 
-	exec { "add application instance 1 ${appname}":
-		require => Exec["add application ${appname}"],
-		command => "/usr/bin/curl -X GET http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/ra/mApplications/${appname}/addInstance&host=localhost"
+	exec { "add application instance 1 ${title}":
+		require => Exec["add application ${title}"],
+		command => "/usr/bin/curl -X GET http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/ra/mApplications/${title}/addInstance&host=localhost"
 	}
 
-	exec { "add application instance 2 ${appname}":
-		require => Exec["add application ${appname}"],
-		command => "/usr/bin/curl -X GET http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/ra/mApplications/${appname}/addInstance&host=localhost"
+	exec { "add application instance 2 ${title}":
+		require => Exec["add application ${title}"],
+		command => "/usr/bin/curl -X GET http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/ra/mApplications/${title}/addInstance&host=localhost"
 	}
 
-	exec { "schedule application instance ${appname}":
-		require => [Exec["add application instance 1 ${appname}"], Exec["add application instance 2 ${appname}"]],
-		command => "/usr/bin/curl -X GET \"http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/admin/turnScheduledOn?type=ins&name=${appname}-1\""
+	exec { "schedule application instance ${title}":
+		require => [Exec["add application instance 1 ${title}"], Exec["add application instance 2 ${title}"]],
+		command => "/usr/bin/curl -X GET \"http://localhost:1086/cgi-bin/WebObjects/JavaMonitor.woa/admin/turnScheduledOn?type=ins&name=${title}-1\""
+	}
+
+	if !defined(File['/var/lib/webobjects/htdocs/WebObjects']) {
+		file { ['/var/lib/webobjects', '/var/lib/webobjects/htdocs', '/var/lib/webobjects/htdocs/WebObjects']:
+			ensure => 'directory',
+		}
+	}
+
+	if !defined(File["/var/lib/webobjects/htdocs/WebObjects/${appname}.woa"]) {
+		file { "/var/lib/webobjects/htdocs/WebObjects/${appname}.woa":
+			ensure	=> 'link',
+			target	=> "/home/${username}/apps/${appname}.woa",
+			require	=> File['/var/lib/webobjects/htdocs/WebObjects'],
+		}
 	}
 }
